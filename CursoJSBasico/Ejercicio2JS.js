78423378e114e3df47f7b45a7fa6c76056a622f6
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2022-11-13 21:04:08 -0500

    ejercicio 2 JS

---
+++ CursoJSBasico/Ejercicio2JS.js
@@ -0,0 +1,12 @@
+var cumple = new Date ("may 1 1995")
+var libro = {
+    nombre: "el aleph",
+    autor: "Jorge Luis Borges",
+    fecha: 1949,
+    url: "https://bcn.gob.ar/uploads/posts/El-Aleph.pdf"
+}
+
+
+let ejercicio = ["Brandon", 27, true, cumple, libro]
+
+
