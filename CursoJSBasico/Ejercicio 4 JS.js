d8d0197676cb1464f9b29ad43067368e299e162b
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2022-11-14 12:27:40 -0500

    ejercicio JS 4

---
+++ CursoJSBasico/Ejercicio 4 JS.js
@@ -0,0 +1,16 @@
+"Brandon"
+"Reyes"
+let estudiante = "Brandon" + " " + "Reyes";
+let estudianteMayus = estudiante.toUpperCase(); 
+let estudianteMinus = estudiante.toLowerCase();
+let numLetras= estudiante.length;
+let primeraLet= estudiante[0];
+let ultimaLet =  estudiante.charAt(estudiante.length-1);
+let sinSpace = estudiante.replace(/ /g, "");
+
+
+let booll = undefined
+    if (estudiante.indexOf("Brandon") !== -1) 
+        booll = true; 
+        else booll = false
+        
