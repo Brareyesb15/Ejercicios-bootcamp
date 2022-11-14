bdbc80f803bcef81c81e2f1eb4ab538bc32d6ef8
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2022-11-14 17:57:17 -0500

    ejercicio 5 js

---
+++ CursoJSBasico/Ejercicio 5 JS.js
@@ -0,0 +1,11 @@
+let altura = 184
+let altMt= 1.84 
+let peso = 84.5
+let altRed= Math.ceil(altMt) 
+let pesoRed= Math.floor(peso)
+let maxValor = undefined
+
+   if (Number.MAX_VALUE +1 === Number.MAX_VALUE) 
+maxValor = true;
+else maxValor = false;
+console.log (maxValor)
