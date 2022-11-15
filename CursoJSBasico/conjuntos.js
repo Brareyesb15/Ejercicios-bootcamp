20778158be3982b54b2d1ff641f7699d1855dba7
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2022-11-15 11:32:19 -0500

    ejercicio conjuntos

---
+++ CursoJSBasico/conjuntos.js
@@ -0,0 +1,5 @@
+let family = ["Wilberto", "Elvia", "Juan","Jose", "Benito","Brandon"]
+let familySet = new Set(family)
+familySet.add ("Brandon")
+familySet.add ("JavaScript")
+console.log (familySet)
