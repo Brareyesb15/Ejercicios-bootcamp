220a0140d0375eefd2e3c0ddc913b35707b297cf
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2022-11-16 22:08:56 -0500

    ejercicio 10 objetos js

---
+++ CursoJSBasico/ejercicios 9 js.js
@@ -0,0 +1,22 @@
+class Estudiante {
+constructor(nombre) {
+    this.nombre = nombre}
+
+
+    asignaturas = ["Javascript", "HTML", "CSS"] 
+
+    obtenDatos() {return this.nombre, this.asignaturas}
+        
+    
+}
+
+
+    let estudiante_1 = new Estudiante("Brandon") 
+    estudiante_1.obtenDatos()
+
+
+
+
+
+
+
